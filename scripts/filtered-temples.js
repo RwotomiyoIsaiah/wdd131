6dfe79c476424347e040d49f64917d47e6584b27
RwotomiyoIsaiah <rwotomiyoisaiah@gmail.com>
2026-09-25 21:49:48 +0300

    u

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -115,28 +115,28 @@ function displayTemples(temples) {
 
 
 const oldLink = document.querySelector("#old");
-oldLink.addEventListener("click", () => {
+oldLink.addEventListener("click", (event) => {event.preventDefault();
     displayTemples(temples.filter(temple => parseInt(temple.dedicated.split(",")[0]) < 1900));
 });
 
 const newLink = document.querySelector("#new");
-newLink.addEventListener("click", () => {
+newLink.addEventListener("click", (event) => {event.preventDefault();
     displayTemples(temples.filter(temple => parseInt(temple.dedicated.split(",")[0]) > 2000));
 });
 
 
 const largeLink = document.querySelector("#large");
-largeLink.addEventListener("click", () => {
+largeLink.addEventListener("click", (event) => {event.preventDefault();
     displayTemples(temples.filter(temple => temple.area > 90000));
 });
 
 const smallLink = document.querySelector("#small");
-smallLink.addEventListener("click", () => {
+smallLink.addEventListener("click", (event) => {event.preventDefault();
     displayTemples(temples.filter(temple => temple.area < 10000));
 });
 
 const homeLink = document.querySelector("#all");
-homeLink.addEventListener("click", () => {
+homeLink.addEventListener("click", (event) => {event.preventDefault();
     displayTemples(temples);
 });
 displayTemples(temples)
